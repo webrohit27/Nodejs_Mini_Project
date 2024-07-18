ee7bcdc0e7c48fbdffb22094c942ad96af72f3d9
rohitrodge <webrohit27@gmail.com>
2024-07-19 01:16:59 +0530

    boiler plate is ready

---
+++ Realtime_Tracker/app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const server = http.createServer(app)
+const io = socketio(server);
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "public")));
+
+io.on("connection", function (socket){
+     console.log("connected");
+})
+
+app.get("/", function (req, res){
+    res.render("index")
+});
+
+server.listen(3000);
---
+++ Realtime_Tracker/public/js/script.js
@@ -0,0 +1,16 @@
+// check if the browser supports geolocation.
+
+const socket = io();
+console.log("ram ram");
+
+// set options for high accuracy , a 5-second timeout, and no caching.
+
+// use watchposition to track the users locations continuosly.
+
+// Initialize a map centered at coordinate (0,0) with a zoom level of 15 using leaflet.
+
+// Add Openstreetmap tiles to map.
+
+// create an empty object markers.
+
+// when receiving location data via the socket, extract id, latitude and longitude
